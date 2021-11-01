8bea4ffe121b738669567e9b00475a5a6091d79d
ThomasMClark <91221549+ThomasMClark@users.noreply.github.com>
2021-11-01 09:39:33 -0500

    Create ifscript.js

---
+++ u5_conditionals/ifscript.js
@@ -0,0 +1,13 @@
+let age=20
+if (age >= 18) {
+    console.log("You are old enough to vote")
+};
+console.log("Thank you for checking your age");
+let comp_sci_grade = 95
+if(comp_sci_grade >= 90) {
+    console.log("you can goto the mall")
+}
+age=14
+if(age<16) { 
+    console.log(" sorry. you are not oldenough to drive")
+}
